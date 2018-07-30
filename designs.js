3e78fad4254973dee58ce6f803b3f1476b224ed0
Your Name <you@example.com>
2018-07-30 22:33:43 +0200

    Draw the grid after submit button is clicked

--- designs.js
+++ designs.js
@@ -5,8 +5,36 @@ const btnSubmit = document.querySelector('input[type=submit]');
 const colorPicker = document.getElementById('colorPicker');
 const pixelCanvas = document.getElementById('pixelCanvas');
 
-
+// event listeners
+// submit button
+btnSubmit.addEventListener('click', function (e) {
+    e.preventDefault();
+    makeGrid();
+});
 
 function makeGrid() {
+    // clear privous grid before drawing the new one
+    clearGrid();
 
+    const grid = document.createDocumentFragment();
+
+    for (let i = 0; i < inputHeight.value; i++) {
+        let gridRow = document.createElement('tr');
+        
+        // create the columns for each row
+        for (let j = 0; j < inputWidth.value; j++) {
+            gridRow.appendChild(document.createElement('td'));
+        }
+
+        // add the row to the grid before creating the next one
+        grid.appendChild(gridRow);
+    }
+
+    pixelCanvas.appendChild(grid);
+}
+
+function clearGrid() {
+    while (pixelCanvas.hasChildNodes()) {   
+        pixelCanvas.removeChild(pixelCanvas.firstChild);
+    }
 }
